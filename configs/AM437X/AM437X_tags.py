7a55f7d04e0ccd35cbe9fa372bc062b4424f7a44
Bin Liu <b-liu@ti.com>
2020-02-18 20:02:52 -0600

    configs: AM437X: change fam_name varible to AM437X
    so that configs/AM437Xx/ settings are taken with build command
    	make DEVFAMILY=AM437X OS=<os>
    
    Signed-off-by: Bin Liu <b-liu@ti.com>

--- configs/AM437X/AM437X_tags.py
+++ configs/AM437X/AM437X_tags.py
@@ -1,4 +1,4 @@
-# Device Family is GEN = General family
-fam_name = 'GEN'
+# Device Family is AM437X = AM437X family
+fam_name = 'AM437X'
 # SDK is general (i.e. not automotive)
 sdk_product = 'general'
