8e015976e91f2f3f015918b25b8979cc3efa4515
Bin Liu <b-liu@ti.com>
2020-09-24 13:21:27 -0500

    check-warn.sh: put temp log files under build/ folder
    to not contaminate the top directory.
    
    Signed-off-by: Bin Liu <b-liu@ti.com>

--- bin/check-warn.sh
+++ bin/check-warn.sh
@@ -79,29 +79,29 @@ fi
 git checkout ${_old} || exit 10
 
 make DEVFAMILY=${_dev} OS=${_os} clean
-make DEVFAMILY=${_dev} OS=${_os} config > _a.log 2>&1 || exit 12
-make DEVFAMILY=${_dev} OS=${_os} >> _a.log 2>&1 || exit 13
-grep "WARNING:" _a.log > _a-warn.log
+make DEVFAMILY=${_dev} OS=${_os} config > build/_a.log 2>&1 || exit 12
+make DEVFAMILY=${_dev} OS=${_os} >> build/_a.log 2>&1 || exit 13
+grep "WARNING:" build/_a.log > build/_a-warn.log
 
 git checkout ${_new} || exit 20
 
 make DEVFAMILY=${_dev} OS=${_os} clean
-make DEVFAMILY=${_dev} OS=${_os} config > _b.log 2>&1 || exit 22
-make DEVFAMILY=${_dev} OS=${_os} >> _b.log 2>&1 || exit 23
-grep "WARNING:" _b.log > _b-warn.log
+make DEVFAMILY=${_dev} OS=${_os} config > build/_b.log 2>&1 || exit 22
+make DEVFAMILY=${_dev} OS=${_os} >> build/_b.log 2>&1 || exit 23
+grep "WARNING:" build/_b.log > build/_b-warn.log
 
 restore_branch
 
 diff --changed-group-format="%>" --unchanged-group-format="" \
-	_a-warn.log _b-warn.log > _new-warn.log
+	build/_a-warn.log build/_b-warn.log > build/_new-warn.log
 
-_num=$(cat _new-warn.log | wc -l)
+_num=$(cat build/_new-warn.log | wc -l)
 
 echo
 echo "Found $_num new build WARNING(s)."
 echo
 
 if [[ $_num != "0" ]]; then
-	cat _new-warn.log
+	cat build/_new-warn.log
 fi
 
