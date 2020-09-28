a17aa0d35254a249698a466e483abeb0434806d3
Hongmei Gou <h-gou@ti.com>
2020-09-28 15:14:08 -0400

    Migrate AM4 (linux and rtos) to the new directory structure
    Signed-off-by: Hongmei Gou <h-gou@ti.com>

--- configs/AM437X/AM437X_linux_tags.py
+++ configs/AM437X/AM437X_linux_tags.py
@@ -1,17 +1,17 @@
-# Device Family is AM335X = AM335X family
-fam_name = 'AM335X'
+# Device Family is AM437X = AM437X family
+fam_name = 'AM437X'
 
 # Project name and HTML title
 sdk_product = 'null' #todo: remove after the new structure is used for all device families
-project = u'Processor SDK RTOS for AM335X'
-html_title = 'Processor SDK RTOS for AM335X Documentation'
+project = u'Processor SDK Linux for AM437X'
+html_title = 'Processor SDK Linux for AM437X Documentation'
 
 # The master toctree document.
-master_doc = 'devices/AM335X/rtos/index'
+master_doc = 'devices/AM437X/linux/index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['linux', 'android', 'rtos/index.rst', 'devices/AM335X/linux', 'devices/AM437X', 'devices/AM64X']
+exclude_patterns = ['rtos', 'android', 'linux/index.rst', 'devices/AM437X/rtos', 'devices/AM335X', 'devices/AM64X']
 
 # OS for the build. Sphinx uses source/{sdk_os} when looking for doc inputs
-sdk_os = 'rtos'
+sdk_os = 'linux'
---
+++ source/devices/AM437X/linux/Release_Specific.rst
@@ -0,0 +1,12 @@
+##################
+Release Specific
+##################
+
+.. toctree::
+   :maxdepth: 5
+
+   Release_Specific_Release_Notes
+   Release_Specific_Performance_Guide
+   /linux/Release_Specific_Yocto_layer_Configuration
+   Release_Specific_Migration_Guide
+   /linux/Release_Specific_Supported_Platforms_and_Versions
