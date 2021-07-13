yogeshhegde/processor-sdk-doc

commit bee5064f962565887fde1a1abedf3237622a60b2

Define version/release from sphinx-build command line
Signed-off-by: Hongmei Gou <a0271529@ti.com>
Signed-off-by: Jacob Stiffler <j-stiffler@ti.com>

Makefile
@@ -20,7 +20,8 @@ endif
 PAPEROPT_a4     = -D latex_paper_size=a4
 PAPEROPT_letter = -D latex_paper_size=letter
 CONFLOC         = -c source/${OS}
-ALLSPHINXOPTS   = -d $(BUILDDIR)/doctrees $(PAPEROPT_$(PAPER)) $(SPHINXOPTS) $(CONFLOC) source
+VEROPTS         = -D version=${VERSION} -D release=${VERSION}
+ALLSPHINXOPTS   = -d $(BUILDDIR)/doctrees $(PAPEROPT_$(PAPER)) $(SPHINXOPTS) $(CONFLOC) $(VEROPTS) source
 # the i18n builder cannot share the environment and doctrees with the others
 I18NSPHINXOPTS  = $(PAPEROPT_$(PAPER)) $(SPHINXOPTS) source
 

make.bat
@@ -9,7 +9,8 @@ set OS=%2
 set /p VERSION=<version.txt
 set BUILDDIR=build/processor-sdk-%OS%/esd/docs/%VERSION%
 set CONFLOC=-c source/%OS%
-set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% %CONFLOC% source
+set VEROPTS=-D version=%VERSION% -D release=%VERSION%
+set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% %CONFLOC% %VEROPTS% source
 set I18NSPHINXOPTS=%SPHINXOPTS% source
 if NOT "%PAPER%" == "" (
 	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%

source/common/conf.py
@@ -48,14 +48,6 @@ source_suffix = '.rst'
 
 copyright = u'1995-2018, Texas Instruments Incorporated. All rights reserved'
 author = u'Texas Instruments'
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-version = 'SDKVERSION'
-# The full version, including alpha/beta/rc tags.
-release = 'SDKVERSION'
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.

commit e116a9192458dc5c0cae86ae87213c8931c16a02

Makefile: remove unnecessary command in make config
'SDKVERSION' in conf.py has been removed in commit bee5064f9625, so this
sed command in make config is no longer needed.

Fixes: bee5064f9625 ("Define version/release from sphinx-build command line")

Signed-off-by: Bin Liu <b-liu@ti.com>

Makefile
@@ -65,7 +65,6 @@ clean:
 config:
 	mkdir -pv build
 	cat python-scripts/conf.py ${OS_CONF} ${TAGFILE} ${FAMILYSETUPFILE} > build/conf.py
-	sed -i 's/SDKVERSION/${VERSION}/g' build/conf.py
 	cp sphinx_rtd_theme_ti/patch/layout.html sphinx_rtd_theme_ti/_themes/sphinx_rtd_theme_ti/layout.html
 
 html: 