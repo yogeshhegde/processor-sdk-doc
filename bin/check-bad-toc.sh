e3fbca7baaea551217dfa86d6eac2ac115b0deaf
Randolph Sapp <rs@ti.com>
2024-01-19 18:13:18 -0600

    bin: check-bad-toc.sh: make sure toc is clean
    Make sure the TOC tree doesn't directly include files that are supposed
    to be included by other files.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

---
+++ bin/check-bad-toc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# make sure the toc tree isn't including files that are supposed to be included
+# by other files
+
+find configs/ -name '*_toc.txt' -print0 | 
+	while IFS= read -r -d '' file_name; do 
+		grep -Pi '.*/_[^\/]*' "$file_name"
+	done
