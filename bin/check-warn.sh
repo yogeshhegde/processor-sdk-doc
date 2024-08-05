7575543cb9c4fe199cbd924263ed72207b82ec28
Randolph Sapp <rs@ti.com>
2024-08-05 17:22:20 -0500

    bin/check-warn.sh: isolate main process from arg parse
    Isolate the main processing from the argument parsing so it's clear
    where we are exiting from.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- bin/check-warn.sh
+++ bin/check-warn.sh
@@ -34,7 +34,73 @@ restore_branch()
 	[ -z "$_cbr" ] || git checkout "$_cbr"
 }
 
-### main() ###
+main()
+{
+	if [[ "$(head -1 Makefile 2> /dev/null)" != "# Makefile for Sphinx"* ]]
+	then
+		echo "Error: Not in the top directory"
+		exit 3
+	fi
+
+	# do nothing if current workspace is not clean
+	if [ -n "$(git status --porcelain --untracked-files=no)" ]
+	then
+		echo "Error: Current workspace has uncommitted changes"
+		exit 4
+	fi
+
+	# do nothing if target ${_new} doesn't exist,
+	# don't wait for it fails after ${_old} was built
+	if ! git cat-file -t "${_new}" > /dev/null 2>&1; then
+		echo "${_new} not found"
+		exit 5
+	fi
+
+	# convert 'HEAD' to its commit ID
+	if [ "$_new" = "HEAD" ]; then
+		_new=$(git rev-parse HEAD)
+	fi
+	if [ "$_old" = "HEAD" ]; then
+		_old=$(git rev-parse HEAD)
+	fi
+
+	# get current branch name or commit ID
+	_cbr=$(git branch | sed -n '/\* /s///p')
+	if [[ "$_cbr" == "(HEAD detached"* ]]; then
+		_cbr=$(git rev-parse HEAD)
+	fi
+
+	git checkout "${_old}" || exit 10
+
+	mkdir -p build
+
+	make DEVFAMILY="${_dev}" OS="${_os}" clean
+	make DEVFAMILY="${_dev}" OS="${_os}" config > build/_a.log 2>&1 || exit 12
+	make DEVFAMILY="${_dev}" OS="${_os}" >> build/_a.log 2>&1 || exit 13
+	grep "WARNING:" build/_a.log > build/_a-warn.log
+
+	git checkout "${_new}" || exit 20
+
+	make DEVFAMILY="${_dev}" OS="${_os}" clean
+	make DEVFAMILY="${_dev}" OS="${_os}" config > build/_b.log 2>&1 || exit 22
+	make DEVFAMILY="${_dev}" OS="${_os}" >> build/_b.log 2>&1 || exit 23
+	grep "WARNING:" build/_b.log > build/_b-warn.log
+
+	restore_branch
+
+	diff --changed-group-format="%>" --unchanged-group-format="" \
+		build/_a-warn.log build/_b-warn.log > build/_new-warn.log
+
+	_num=$(wc -l build/_new-warn.log)
+
+	echo
+	echo "Found $_num new build WARNING(s)."
+	echo
+
+	if [ "$_num" != "0" ]; then
+		cat build/_new-warn.log
+	fi
+}
 
 while [ "$#" -gt 0 ]; do
 	case $1 in
@@ -53,68 +119,4 @@ if [ -z "$_old" ] || [ -z "$_new" ]; then
 fi
 [ -n "$_os" ] || _os=linux
 
-if [[ "$(head -1 Makefile 2> /dev/null)" != "# Makefile for Sphinx"* ]]
-then
-	echo "Error: Not in the top directory"
-	exit 3
-fi
-
-# do nothing if current workspace is not clean
-if [ -n "$(git status --porcelain --untracked-files=no)" ]
-then
-	echo "Error: Current workspace has uncommitted changes"
-	exit 4
-fi
-
-# do nothing if target ${_new} doesn't exist,
-# don't wait for it fails after ${_old} was built
-if ! git cat-file -t "${_new}" > /dev/null 2>&1; then
-	echo "${_new} not found"
-	exit 5
-fi
-
-# convert 'HEAD' to its commit ID
-if [ "$_new" = "HEAD" ]; then
-	_new=$(git rev-parse HEAD)
-fi
-if [ "$_old" = "HEAD" ]; then
-	_old=$(git rev-parse HEAD)
-fi
-
-# get current branch name or commit ID
-_cbr=$(git branch | sed -n '/\* /s///p')
-if [[ "$_cbr" == "(HEAD detached"* ]]; then
-	_cbr=$(git rev-parse HEAD)
-fi
-
-git checkout "${_old}" || exit 10
-
-mkdir -p build
-
-make DEVFAMILY="${_dev}" OS="${_os}" clean
-make DEVFAMILY="${_dev}" OS="${_os}" config > build/_a.log 2>&1 || exit 12
-make DEVFAMILY="${_dev}" OS="${_os}" >> build/_a.log 2>&1 || exit 13
-grep "WARNING:" build/_a.log > build/_a-warn.log
-
-git checkout "${_new}" || exit 20
-
-make DEVFAMILY="${_dev}" OS="${_os}" clean
-make DEVFAMILY="${_dev}" OS="${_os}" config > build/_b.log 2>&1 || exit 22
-make DEVFAMILY="${_dev}" OS="${_os}" >> build/_b.log 2>&1 || exit 23
-grep "WARNING:" build/_b.log > build/_b-warn.log
-
-restore_branch
-
-diff --changed-group-format="%>" --unchanged-group-format="" \
-	build/_a-warn.log build/_b-warn.log > build/_new-warn.log
-
-_num=$(wc -l build/_new-warn.log)
-
-echo
-echo "Found $_num new build WARNING(s)."
-echo
-
-if [ "$_num" != "0" ]; then
-	cat build/_new-warn.log
-fi
-
+main
