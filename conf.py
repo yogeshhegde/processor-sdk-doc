50c27d1675e6c0fd874134cb5835ec54439926ab
Randolph Sapp <rs@ti.com>
2024-11-19 01:56:56 -0600

    fix(conf): remove unnecessary source files
    No need to create these artifacts. We don't do anything with them and
    the repo source is publicly available anyway.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- conf.py
+++ conf.py
@@ -163,6 +163,9 @@ html_additional_pages = {'index': 'index.html'}
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
 
+# If True, the reStructuredText sources are included in the HTML build as _sources/docname.
+html_copy_source = False
+
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
 
