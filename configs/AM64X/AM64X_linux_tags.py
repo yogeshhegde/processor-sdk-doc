02d7f002c57e84f66a1eee880d55cc3ea910a812
Randolph Sapp <rs@ti.com>
2025-05-01 15:35:24 -0500

    chore: remove unreachable example files
    Remove the example_code directory and all mentions of it. This directory
    holds an isolated RTOS related example with no references to it in the
    main documentation.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- configs/AM64X/AM64X_linux_tags.py
+++ configs/AM64X/AM64X_linux_tags.py
@@ -15,7 +15,7 @@ master_doc = 'devices/AM64X/index'
 
 # AM64x RTOS docs are not in this project, rather referenced externally,
 # so exclude 'rtos' folder along with others.
-exclude_patterns = ['android', 'example_code', 'files', 'rtos', 'devices/AM335X', 'devices/AM437X', 'devices/AM65X', 'devices/J7_Family', 'devices/DRA821A' , 'devices/AM62X', 'devices/AM62AX', 'devices/AM62PX', 'devices/AM62LX']
+exclude_patterns = ['android', 'files', 'rtos', 'devices/AM335X', 'devices/AM437X', 'devices/AM65X', 'devices/J7_Family', 'devices/DRA821A' , 'devices/AM62X', 'devices/AM62AX', 'devices/AM62PX', 'devices/AM62LX']
 
 # OS for the build. Sphinx uses source/{sdk_os} when looking for doc inputs
 sdk_os = 'linux'
